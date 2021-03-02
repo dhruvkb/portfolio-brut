d4f9f69dac7a7863d486bf3703f29e8a606d9c6b
Dhruv Bhanushali <dhruv_b@live.com>
2021-03-02 23:31:57 +0530

    Add a component to display a randomly generated kaomoji face

---
+++ src/components/landing/Kaomoji.vue
@@ -0,0 +1,49 @@
+<template>
+  <span>{{ face }}ﾉ</span>
+</template>
+
+<script>
+  import sample from 'lodash/sample'
+
+  export default {
+    name: 'Kaomoji',
+    data() {
+      return {
+        edgesList: [
+          ['(', ')'],
+        ],
+        eyesList: [
+          ['ˇ', 'ˇ'],
+          ['❛', '❛'],
+          ['^', '^'],
+          ['＾', '＾'],
+          ['´•', '•`'],
+          ['◕', '◕'],
+        ],
+        mouthList: [
+          '◡',
+          'ᴗ',
+          'ᵕ',
+          '▽',
+          'ヮ',
+          '*',
+        ],
+      }
+    },
+    computed: {
+      face() {
+        const [edgeLeft, edgeRight] = sample(this.edgesList)
+        const [eyeLeft, eyeRight] = sample(this.eyesList)
+        const mouth = sample(this.mouthList)
+
+        return [
+          edgeLeft,
+          eyeLeft,
+          mouth,
+          eyeRight,
+          edgeRight,
+        ].join(' ')
+      },
+    },
+  }
+</script>
