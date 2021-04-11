3ab433d056d98bf820068fcb26245e1ba3c45880
Dhruv Bhanushali <dhruv_b@live.com>
2021-04-11 12:26:01 +0530

    Prevent prompt from activating when terminal is not ready

--- src/components/detail/Present.vue
+++ src/components/detail/Present.vue
@@ -1,15 +1,22 @@
 <template>
-  <label class="present">
-    <Prompt/>
-    <input
-      v-model="command"
-      type="text"
-      @keyup.enter="executeCommand">
-  </label>
+  <div class="present">
+    <template v-if="isReady">
+      <label>
+        <Prompt/>
+        <input
+          v-model="command"
+          type="text"
+          @keyup.enter="executeCommand">
+      </label>
+    </template>
+    <template v-else>
+      Processing...
+    </template>
+  </div>
 </template>
 
 <script>
-  import { mapActions } from 'vuex'
+  import { mapActions, mapState } from 'vuex'
 
   import Prompt from '@/components/detail/Prompt.vue'
 
@@ -22,6 +29,11 @@
       return {
         command: '',
       }
+    },
+    computed: {
+      ...mapState('terminal', [
+        'isReady',
+      ]),
     },
     methods: {
       ...mapActions('terminal', [
