8e508a4287273b462718596adadee55b7c6587dc
Dhruv Bhanushali <dhruv_b@live.com>
2021-04-11 15:02:53 +0530

    Move Solarized colors into their separate file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,12 @@
 const colours = require('tailwindcss/colors')
 const screenSizes = require('./src/constants/screen_sizes')
+const solarizedColours = require('./src/constants/colours')
 
 const macSystemSansSerif = [
   'system-ui',
   '-apple-system',
   'BlinkMacSystemFont',
 ]
-
-const solarizedColours = {
-  base: {
-    '03': '#002b36', // L* 15
-    '02': '#073642', // L* 20
-    '01': '#586e75', // L* 45
-    '00': '#657b83', // L* 50
-    0: '#839496', // L* 60
-    1: '#93a1a1', // L* 65
-    2: '#eee8d5', // L* 92
-    3: '#fdf6e3', // L* 97
-  },
-  accent: {
-    y: '#b58900', // yellow; Split comp
-    o: '#cb4b16', // orange; Complement
-    r: '#dc322f', // red; Triad
-    m: '#d33682', // magenta; Tetrad
-    v: '#6c71c4', // violet; Analogous
-    b: '#268bd2', // blue; Monotone
-    c: '#2aa198', // cyan; Analogous
-    g: '#859900', // green; Tetrad
-  },
-}
 
 module.exports = {
   mode: 'jit',
@@ -63,8 +41,8 @@ module.exports = {
       white: colours.white,
       black: colours.black,
       sol: {
-        ...solarizedColours.accent,
-        ...solarizedColours.base,
+        ...solarizedColours.colors.accent,
+        ...solarizedColours.colors.base,
       },
     },
     fontFamily: {
