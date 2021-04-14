5061a156bd73039147f26b9fc5c4481ba3508ea7
Dhruv Bhanushali <dhruv_b@live.com>
2021-04-14 21:13:46 +0530

    Create the component functioning as the `tree` binary

---
+++ src/components/bin/Tree.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="tree">
+    <slot/>
+    {{ shape }}
+    <Navigable :node="node"/>
+    <Tree
+      v-for="(child, index) in node.children"
+      :key="index"
+      :argv="[absolutePathTo(child)]">
+      <slot/>
+      {{ childShape }}
+    </Tree>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  import Navigable from '@/components/detail/Navigable.vue'
+
+  import bin from '@/mixins/bin'
+
+  export default {
+    name: 'Tree',
+    command: 'tree',
+    description: 'List contents of directory recursively like a tree',
+    argSpec: {
+      posArgs: [
+        {
+          name: 'dirname',
+          description: 'the directory of which to list contents',
+          default: '.',
+        },
+      ],
+    },
+    mixins: [
+      bin,
+    ],
+    components: {
+      Navigable,
+    },
+    data() {
+      return {
+        pipe: '│',
+        tee: '├',
+        bend: '└',
+        dash: '─',
+        space: '\xa0',
+      }
+    },
+    computed: {
+      dir() {
+        return this.nodeLocatedAt(this.args.dirname.replace(/\/$/, ''))
+      },
+      isFound() {
+        return this.node && this.node.isFolder
+      },
+      isRoot() {
+        return !this.$slots.default
+      },
+      isLastChild() {
+        const siblings = this.node.parent.children
+        return siblings.indexOf(this.node) === siblings.length - 1
+      },
+      shape() {
+        if (this.isRoot) {
+          return ''
+        }
+        if (this.isLastChild) {
+          return [this.bend, this.dash].join('')
+        }
+        return [this.tee, this.dash].join('')
+      },
+      childShape() {
+        if (this.isRoot) {
+          return ''
+        }
+        if (this.isLastChild) {
+          return [this.space, this.space].join('')
+        }
+        return [this.pipe, this.space].join('')
+      },
+      ...mapGetters('terminal', [
+        'nodeLocatedAt',
+        'absolutePathTo',
+      ]),
+    },
+    created() {
+      this.node = this.dir
+    },
+  }
+</script>
--- src/components/bin/registry.js
+++ src/components/bin/registry.js
@@ -5,6 +5,7 @@ import Help from '@/components/bin/Help.vue'
 import Intro from '@/components/bin/Intro.vue'
 import List from '@/components/bin/List.vue'
 import Manual from '@/components/bin/Manual.vue'
+import Tree from '@/components/bin/Tree.vue'
 import WhoAmI from '@/components/bin/WhoAmI.vue'
 import WorkDir from '@/components/bin/WorkDir.vue'
 
@@ -16,6 +17,7 @@ export const components = {
   Intro,
   List,
   Manual,
+  Tree,
   WhoAmI,
   WorkDir,
 }
