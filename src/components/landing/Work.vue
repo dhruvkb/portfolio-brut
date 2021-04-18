d341b3e9a1f4b8d9dcbefb9a95fd54ce71fcf1f8
Dhruv Bhanushali <dhruv_b@live.com>
2021-04-18 12:31:01 +0530

    Replace tables with responsive grids

--- src/components/landing/Work.vue
+++ src/components/landing/Work.vue
@@ -1,32 +1,27 @@
 <template>
-  <table class="work table-fixed w-full text-sol-00 hover:text-sol-1">
-    <thead class="border-b border-sol-1">
-      <tr class="text-xs uppercase">
-        <th
-          v-for="(column, index) in columns"
-          :key="index"
-          class="text-left font-semibold text-sol-1 py-1 px-2 s:first:pl-2 s:last:pr-2 m:first:pl-4 m:last:pr-2 l:first:pl-8 l:last:pr-8 sx:first:pl-2 sx:last:pr-2 mx:first:pl-4 mx:last:pr-4"
-          :data-col="column.fieldName">
-          {{ column.displayName }}
-        </th>
-      </tr>
-    </thead>
-    <tbody class="text-lg border-b border-sol-1">
-      <tr
-        v-for="(project, storyIndex) in projects"
-        :key="storyIndex"
-        class="hover:text-sol-01 hover:bg-sol-2 transition-colors duration-250 outline-none"
-        role="button"
-        tabindex="0">
-        <td
-          v-for="(column, index) in columns"
-          :key="index"
-          class="py-1 px-2 s:first:pl-2 s:last:pr-2 m:first:pl-4 m:last:pr-2 l:first:pl-8 l:last:pr-8 sx:first:pl-2 sx:last:pr-2 mx:first:pl-4 mx:last:pr-4">
-          {{ project[column.fieldName] }}
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="work border-b border-sol-1 text-sol-00 hover:text-sol-1">
+    <div class="works grid font-semibold text-xs uppercase text-sol-1 border-b border-sol-1">
+      <div
+        v-for="(column, columnIndex) in columns"
+        :key="columnIndex"
+        class="resume-cell">
+        {{ column.displayName }}
+      </div>
+    </div>
+    <div
+      v-for="(project, projectIndex) in projects"
+      :key="projectIndex"
+      class="works grid text-lg hover:text-sol-01 hover:bg-sol-2 transition-colors"
+      role="button"
+      tabindex="0">
+      <div
+        v-for="(column, columnIndex) in columns"
+        :key="columnIndex"
+        class="resume-cell">
+        {{ project[column.fieldName] }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -83,53 +78,23 @@
 </script>
 
 <style scoped lang="css">
-  @screen s {
-    .work {
-      --work-epicName-width: 40%;
-      --work-title-width: 60%;
+  .works {
+    @screen s {
+      --works-cols: 40% 60%;
     }
-  }
-
-  @screen m {
-    .work {
-      --work-epicName-width: 20%;
-      --work-title-width: 30%;
-      --work-technologies-width: 50%;
+    @screen m {
+      --works-cols: 20% 30% 50%;
     }
-  }
-
-  @screen l {
-    .work {
-      --work-epicName-width: 27.5%;
-      --work-title-width: 22.5%;
-      --work-technologies-width: 50%;
+    @screen l {
+      --works-cols: 27.5% 22.5% 50%;
     }
-  }
-
-  @screen sx {
-    .work {
-      --work-epicName-width: 40%;
-      --work-title-width: 60%;
+    @screen sx {
+      --works-cols: 40% 60%;
     }
-  }
-
-  @screen mx {
-    .work {
-      --work-epicName-width: 20%;
-      --work-title-width: 30%;
-      --work-technologies-width: 50%;
+    @screen mx {
+      --works-cols: 20% 30% 50%;
     }
-  }
 
-  .work th[data-col="epicName"] {
-    width: var(--work-epicName-width, inherit);
-  }
-
-  .work th[data-col="title"] {
-    width: var(--work-title-width, inherit);
-  }
-
-  .work th[data-col="technologies"] {
-    width: var(--work-technologies-width, inherit);
+    grid-template-columns: var(--works-cols);
   }
 </style>
