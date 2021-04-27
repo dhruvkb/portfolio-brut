9852e67d0d8eeb3c20ac72324cabd21de523aa5a
Dhruv Bhanushali <dhruv_b@live.com>
2021-04-27 14:25:45 +0530

    Create a new component for displaying SVG given all the paths

---
+++ src/components/landing/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    class="h-4 w-4"
+    viewBox="0 0 24 24">
+    <path
+      v-for="(path, index) in paths"
+      :key="index"
+      v-bind="path"/>
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: 'Icon',
+    props: {
+      paths: {
+        type: Array,
+        required: true,
+      },
+    },
+  }
+</script>
