273454f0248910415726c35376ccdd64bdd4a2f0
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-21 13:58:10 +0530

    Revamp detail section using terminal from see·el·aye

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,29 +1,100 @@
 <template>
-  <div class="detail flex flex-col text-sol-0 bg-sol-03 min-h-full">
+  <div class="detail flex flex-col min-h-full">
     <Terminal
-      v-if="breakpoint.name.includes('x')"
-      class="flex-grow"/>
+      v-if="isTerminalVisible"
+      class="flex-grow solarized dark"/>
   </div>
 </template>
 
 <script>
-  import Terminal from '@/components/detail/Terminal.vue'
+  import { computed, onBeforeUnmount } from 'vue'
+  import { useStore } from 'vuex'
+  import {
+    FsNode,
+    FsNodeType,
+    getVuexModule,
+    Terminal,
+    useSeeelaye,
+  } from 'seeelaye'
 
   import { breakpoint } from '@/plugins/responsive'
+
+  import experience from '@/data/experience.json'
+  import work from '@/data/work.json'
 
   export default {
     name: 'Detail',
     components: {
       Terminal,
     },
-    data() {
+    setup() {
+      const seeelaye = useSeeelaye()
+
+      const isTerminalVisible = computed(() => breakpoint.name.includes('x'))
+
+      // Attach and detach see·el·aye module
+      const store = useStore()
+      const terminalModule = getVuexModule()
+      if (!store.hasModule('terminal')) {
+        store.registerModule('terminal', terminalModule)
+      }
+      onBeforeUnmount(() => {
+        if (store.hasModule('terminal')) {
+          store.unregisterModule('terminal')
+        }
+      })
+
+      // Run startup commands when terminal is ready
+      let index = 0
+      const startupCommands = ['intro', 'help']
+      const unsubscribe = seeelaye.store.subscribe((mutation) => {
+        if (mutation.type.includes('/setIsReady') && mutation.payload.isReady) {
+          const command = startupCommands[index]
+          if (command) {
+            index += 1
+            seeelaye.dispatch('executeCmd', { rawInput: command })
+          }
+          if (index === startupCommands.length) {
+            unsubscribe()
+          }
+        }
+      })
+
+      // Generate and populate FS tree
+      const fsBridge = (jsonNode) => {
+        const fsNode = {
+          name: jsonNode.slug,
+          aliases: jsonNode.aliases ?? [],
+        }
+        if (jsonNode.children) {
+          fsNode.children = jsonNode.children.map((child) => fsBridge(child))
+        }
+        return fsNode
+      }
+      const makeTree = () => {
+        const rootNode = new FsNode(FsNodeType.FOLDER, '~')
+        const preBridgeNodes = [experience, work]
+        preBridgeNodes.forEach((preBridgeNode) => {
+          const node = FsNode.parse(fsBridge(preBridgeNode))
+          node.parent = rootNode
+          rootNode.children.push(node)
+        })
+        return rootNode
+      }
+      const tree = makeTree()
+      seeelaye.commit('setTree', { tree })
+      seeelaye.commit('setCurrentNode', { currentNode: tree })
+
+      seeelaye.commit('setIsReady', { isReady: true })
+
       return {
-        breakpoint,
+        isTerminalVisible,
       }
     },
   }
 </script>
 
+<style scoped lang="css" src="seeelaye/dist/themes/solarized.css"/>
 <style scoped lang="css">
   .detail {
     padding-right: env(safe-area-inset-right);
@@ -31,5 +102,13 @@
     & ::selection {
       @apply text-sol-1 bg-sol-02;
     }
+  }
+
+  /* Terminal style variables */
+  ::v-deep(.terminal) {
+    --content-max-width: 80ch;
+    --content-border-width: 1px;
+
+    --folder-color: var(--color-accent-v);
   }
 </style>
