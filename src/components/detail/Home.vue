9db1f615d02496e5796b40b521e4ebbefd12030b
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-26 23:01:25 +0530

    Create a home page with more prominent kaomoji

---
+++ src/components/detail/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="help flex flex-col items-center justify-center text-sol-1">
+    <Kaomoji class="text-2xl mb-2"/>
+    <p class="mb-4">Welcome to my slice of the Internet.</p>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  import Kaomoji from '@/components/detail/Kaomoji.vue'
+
+  export default defineComponent({
+    name: 'Home',
+    components: {
+      Kaomoji,
+    },
+  })
+</script>
