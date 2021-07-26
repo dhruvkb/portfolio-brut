0798f28b8279293732b9afef84702ff61cb8da11
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-26 23:00:18 +0530

    Create a details pane page about me

---
+++ src/components/detail/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home flex flex-col">
+    <Intro/>
+    <Bio/>
+    <Contact class="mt-auto"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  import Intro from '@/components/detail/Intro.vue'
+  import Bio from '@/components/detail/Bio.vue'
+  import Contact from '@/components/detail/Contact.vue'
+
+  export default defineComponent({
+    name: 'Hello',
+    components: {
+      Intro,
+      Bio,
+      Contact,
+    },
+  })
+</script>
